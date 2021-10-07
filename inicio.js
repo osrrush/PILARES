5b5ec2f62645049952a32e2cc0d3d80021fa854a
osrrush <34876091+osrrush@users.noreply.github.com>
2021-10-07 11:07:14 -0500

    Delete inicio.js

--- inicio.js
+++
@@ -1,9 +0,0 @@
-const fs = require(fs);
-const archivo = 'prueba.txt';
-
-var contenido = "Primera linea";
-fs.writeFile(archivo,contenido);
-console.log("Contenido escrito");
-
-fs.appendFile(archivo,"Segunda linea");
-console.log("Contenido escrito");
